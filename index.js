cffd41d1e81f621d860c78d91365f7e4a726add5
Molo Xiao <molo.xiao@gmail.com>
2017-01-19 14:44:30 +0800

    pdfkit可用

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 'use strict';
 
+// https://cnodejs.org/topic/535f3569c0733ff512016a52
+// http://pdfkit.org/docs/getting_started.html
+// https://github.com/devongovett/pdfkit
+
+var PDFDocument = require('pdfkit');
+var doc = new PDFDocument();
+
 var that = {};
 
 
-that.init = function() {
+that.test = function() {
+    var fs = require('fs');
+    doc.pipe(fs.createWriteStream('output.pdf'));
+    // Add another page
+    doc.addPage()
+        .fontSize(25)
+        .text('Here is some vector graphics...', 100, 100);
 
+    // Draw a triangle
+    doc.save()
+        .moveTo(100, 150)
+        .lineTo(100, 250)
+        .lineTo(200, 250)
+        .fill("#FF3300");
+
+    doc.end();
 }
 
 
 
 
 module.exports = that;
+
+that.test();
